222a2b2a043ab371eff8f17eb82ab9be785b7991
Leeson0202 <asedrfa@163.com>
2023-02-10 17:15:36 +0800

    [vue] navigation center 标签完成

--- AppStore-vue/src/pages/Home.vue
+++ AppStore-vue/src/pages/Home.vue
@@ -2,49 +2,33 @@
     <div class="Home">
         <!-- 推荐轮播图 -->
         <Swrapper :apps="apps"></Swrapper>
-
-        <!-- 热门-->
-        <div class="hot">
-            <!--应用下载榜首-->
-            <div class="app-top">
-                <span>下载榜单</span>
-                <el-tabs tab-position="top" style="height: 200px; ">
-                    <el-tab-pane label="今日">
-                        <el-col style="margin-top: 6px" :span="24" v-for="(item, index) in apps" :key="index"
-                                :offset="0">
-                            <el-image class="top-icon"
-                                      :src="getIcon(item.icon)"
-                                      alt=""></el-image>
-                            <div style="float: left; height: 100%;margin-left: 10px; width: calc(100% - 70px);">
-                                <span class="item-name"> {{ item.name }}</span></div>
-                            <hr>
-                        </el-col>
-                    </el-tab-pane>
-                    <el-tab-pane label="本周">配置管理</el-tab-pane>
-                    <el-tab-pane label="本月">角色管理</el-tab-pane>
-                    <el-tab-pane label="本季">定时任务补偿</el-tab-pane>
-                </el-tabs>
-
-            </div>
+        <CodeView></CodeView>
+        <div class="video-block" :style="{height: videoHeight + 'px'}">
+            <video class="video" x5-playsinline webkit-playsinline="true" playsinline="true" muted="true" autoplay>
+                <source :src="require('@/assets/video/Air M2.mp4')">
+<!--                <source src="https://www.apple.com.cn/105/media/cn/mac/2022/56a714a6-7476-4896-b369-0ee5fa649290/films/macbook-pro-14-and-16-mac-mini-product/mac-macbook-pro-14-and-16-mac-mini-product-tpl-cn-2022_16x9.m3u8">-->
+            </video>
 
         </div>
+
     </div>
 </template>
 
 <script>
 import {mapState, mapActions} from "vuex";
 import Swrapper from "@/components/home/Swrapper";
-import SwrapperAbout from "@/store/module/home/SwrapperAbout";
+import CodeView from "@/components/home/code/CodeView.vue";
 
 
 export default {
     name: "Home",
-    components: {Swrapper},
+    components: {Swrapper, CodeView},
     data() {
         //这里存放数据
         return {
             activeName: 'first',
-            src: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            src: "https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
+            videoHeight: 100,
         };
     },
     //监听属性 类似于data概念
@@ -71,6 +55,12 @@ export default {
     },
     //生命周期 - 挂载完成（可以访问DOM元素）
     mounted() {
+        this.videoHeight = document.getElementsByClassName('video')[0].clientHeight + 60;
+        window.onresize = () => {
+            return (() => {
+                this.videoHeight = document.getElementsByClassName('video')[0].clientHeight;
+            })();
+        };
 
     },
     beforeCreate() {
@@ -91,53 +81,28 @@ export default {
 </script>
 
 <style scoped>
-
-
-.hot {
+.video-block {
+    border-radius: 6px;
+    overflow: hidden;
     width: 100%;
-    height: 600px;
-    background-color: #f3f3f3;
 }
 
-.app-top {
-    margin: 0 5px 0 5px;
-    height: 100%;
-    width: 36%;
-    max-width: 250px;
-    overflow: scroll;
-}
-
-.app-top > span {
-    font-size: 16px;
-    font-weight: bold;
-}
-
-.app-top::-webkit-scrollbar {
-    display: none; /* Chrome Safari */
-}
-
-
-.el-col{
-    margin-top: 1px;
-}
-.top-icon {
-    float: left;
-    height: 58px;
-    width: 58px;
-    border-radius: 4px;
-
-}
-
-.item-name {
-    font-size: 14px;
-    line-height: 60px;
-}
-
-hr {
-    margin: 0;
+.video {
+    display: inline-block;
+    border-radius: 6px;
+    position: relative;
     width: 100%;
-    background-color: #adaaaa;
-    height: 1px;
-    border: none;
 }
+
+.video-top {
+    display: inline-block;
+    position: relative;
+    /*background: #8c939d;*/
+    width: 100%;
+    border-radius: 6px;
+    backdrop-filter: blur(0.5px);
+
+}
+
+
 </style>
